0a8b7ffe2809b93ff18b4b82731b0314871ad43b
Zed-Bailey <emailzed03@gmail.com>
2025-07-26 13:27:05 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+function calculateData(e) {
+  const form = document.getElementById("calculateForm");
+  const data = Object.fromEntries(new FormData(form));
+
+  const stake = Number.parseFloat(data["stake"]);
+  // commission should be converted to a float
+  const commission = Number.parseFloat(data["betfair_commission"]) / 100;
+  const layOdds = Number.parseFloat(data["betfair_lay_odds"]);
+  const backOdds = Number.parseFloat(data["bookmaker_back_odds"]);
+
+  const layStake = (backOdds * stake) / (layOdds - commission);
+  const layLiability = layStake * (layOdds - 1);
+  const backProfit = backOdds * stake - stake - layLiability;
+  const layProfit = layStake - layStake * commission - stake;
+
+  updateElementById("profit", `$${backProfit.toFixed(2)}`);
+  updateElementById("lay_stake", `$${layStake.toFixed(2)}`);
+  updateElementById("lay_liability", `$${layLiability.toFixed(2)}`);
+
+  return false;
+}
+
+function updateElementById(name, value) {
+  const ele = document.getElementById(name);
+  if (ele) {
+    ele.innerHTML = value;
+  }
+}
+
+window.onload = () => {
+  const form = document.getElementById("calculateForm");
+
+  form.onsubmit = calculateData;
+};
